9092f4fa6a6210fb722b29a8ed3ff60f993ab0a9
nahooshh <aunindo.ee@gmail.com>
2015-01-22 19:09:26 +0530

    indexpage search bar autocomplete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl',['$rootScope','$scope','localStorageService', function ($rootScope, $scope, localStorageService) {
+  .controller('MainCtrl',['$rootScope','$scope','$http','localStorageService', function ($rootScope, $scope, $http, localStorageService) {
 		//localStorageService.clearAll();
 		$rootScope.tab=true;
 		console.log(1);
@@ -24,4 +24,37 @@ angular.module('frontendApp')
 		$scope.$watch('name', function () {
   		localStorageService.set('name', $scope.name);
 		}, true);
+
+		$( "#modelsearch" ).autocomplete({
+			source: "http://192.168.1.2/autocomplete.php",
+			select: function( event, ui ) {
+				handle_select(ui.item.label);
+			}
+		});
+
+		function handle_select(label) {
+			var i = label.indexOf(" ");
+			var j = label.indexOf("(");
+			var l = label.length;
+
+			var make = label.substring(0,i);
+			var model = '';
+			var type = '';
+			if (j == -1) {
+				model = label.substring(i+1,l);
+			} else {
+				model = label.substring(i+1,j-1);
+				type = label.substring(j,l);
+			}
+			
+			if (type=='') {
+				var qs="http://192.168.1.2/getdata.php".concat("?make=",make,"&model=",model);
+			}
+			else {
+				var qs="http://192.168.1.2/getdata.php".concat("?make=",make,"&model=",model,"&type=",type);
+			}
+			console.log(qs);
+			$http.get(qs).success(function(response) {console.log(response);});
+		}
+
   }]);
