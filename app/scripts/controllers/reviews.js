2a590f9278cac76c98787f488d6fbbe8bcc96f07
nahooshh <aunindo.ee@gmail.com>
2015-04-10 18:33:26 +0530

    tags arrangement

--- app/scripts/controllers/reviews.js
+++ app/scripts/controllers/reviews.js
@@ -33,7 +33,7 @@ angular.module('frontendApp')
 			Dataservice.rev_col=$("#revcol").val();
 			reload();
 		});
-		console.log($("#revcol").val());
+		//console.log($("#revcol").val());
 
 		function reload() {
 			$("#revBattery").empty();
@@ -90,7 +90,7 @@ angular.module('frontendApp')
 					
 		function fillbody2(response) {
 				if (response.length == 0) {
-					line="<p>No Data Found</p>";
+					var line="<p>No Data Found</p>";
 					$("#revBattery").append(line);
 					$("#revBody").append(line);
 					$("#revCamera").append(line);
@@ -101,7 +101,7 @@ angular.module('frontendApp')
 					return;
 				}
 				if (response[0]==false) {
-					line="<p>Please Select a Model</p>";
+					var line="<p>Please Select a Model</p>";
 					$("#revBattery").append(line);
 					$("#revBody").append(line);
 					$("#revCamera").append(line);
@@ -111,13 +111,43 @@ angular.module('frontendApp')
 					$("#revOverall").append(line);
 					return;
 				}
+
+
+				var revData={'batt':[[],[],0,0,0,'#revBattery'],'body':[[],[],0,0,0,'#revBody'],'cam':[[],[],0,0,0,'#revCamera'],'disp':[[],[],0,0,0,'#revDisplay'],'perf':[[],[],0,0,0,'#revPerformance'],'spec':[[],[],0,0,0,'#revSpecs'],'ovr':[[],[],0,0,0,'#revOverall']}
+
 				for (var i=0; i<response.length;i++) {
 					var fea=response[i][0];
 					if (fea=="SUMMARY_RC") {fea="recommendation";}
 					if (fea=="SUMMARY_PRICE") {fea="price";}
-					var line='<p>'.concat("<b>", fea.replace(/_/g," "), "</b>", ' : ');
+
+					var section='';
+					if ((fea == "battery") || (fea == "battery_life")) {
+						section='batt';
+					}
+					else if ((fea == "camera") || (fea == "camera_resolution") || (fea == "camera_flash") || (fea == "picture_quality") || (fea == "video_quality") || (fea == "low_light_performance") || (fea == "secondary_camera") || (fea == "primary_camera") ) {
+						section='cam';
+					}
+					else if ((fea == "built_quality") || (fea == "body_covers") || (fea == "buttons")) {
+						section='body';
+					}
+					else if ((fea == "display") || (fea == "display_size") || (fea == "display_resolution") || (fea == "viewing_angle") || (fea == "touch_response")) {
+						section='disp';
+					}
+					else if ((fea == "performance") || (fea == "apps") || (fea == "video") || (fea == "music") || (fea == "gaming") || (fea=="photography") || (fea == "sound_quality")) {
+						section='perf';
+					}
+					else if ((fea == "headset") || (fea == "processor") || (fea == "gpu") || (fea == "ram") || (fea == "internal_memory")) {
+						section='spec';
+					}
+					else if ((fea == "phone") || (fea == "recommendation") || (fea == "price")) {
+						section='ovr';
+					}
+					else {continue;}
+
+
+					var elems=[]
 					for (var j=1; j<response[i].length;j++) {
-						var tone="black";
+						var tone="grey";
 						var adj=response[i][j][0];
 						if (adj.search("pos_") != -1) {
 							adj=adj.replace("pos_","");
@@ -132,39 +162,72 @@ angular.module('frontendApp')
 						adj=adj.replace(/_/g," ");
 				
 						var dat=response[i][j][1].split(',');
-						var num=dat[0];
-						var href="revdata/revver_1/".concat(dat[1]);	
-						adj=adj.concat('(', num,')');
-						var add="<a href=\"".concat(href, "\" style=\"color:", tone, "\" data-toggle=\"modal\" data-target=\"#myModal\">  ", adj, "  </a>");
-						line=line.concat(add);
+						var num=parseInt(dat[0]);
+						var href="revdata/revver_1/".concat(dat[1]);
+						elems.push([num,adj,href,tone]);
+						revData[section][1].push(num);
+						if (tone=="green") {revData[section][2]=revData[section][2]+num;}
+						if (tone=="grey") {revData[section][3]=revData[section][3]+num;}
+						if (tone=="red") {revData[section][4]=revData[section][4]+num;}
 					}
-					line=line.concat('</p>');
-					//console.log(line);
-
-					if ((fea == "battery") || (fea == "battery_life")) {
-						$("#revBattery").append(line);
-					}
-					else if ((fea == "camera") || (fea == "camera_resolution") || (fea == "camera_flash") || (fea == "picture_quality") || (fea == "video_quality") || (fea == "low_light_performance") || (fea == "secondary_camera") || (fea == "primary_camera") ) {
-						$("#revCamera").append(line);
-					}
-					else if ((fea == "built_quality") || (fea == "body_covers") || (fea == "buttons")) {
-						$("#revBody").append(line);
-					}
-					else if ((fea == "display") || (fea == "display_size") || (fea == "display_resolution") || (fea == "viewing_angle") || (fea == "touch_response")) {
-						$("#revDisplay").append(line);
-					}
-					else if ((fea == "performance") || (fea == "apps") || (fea == "video") || (fea == "music") || (fea == "gaming") || (fea=="photography") || (fea == "sound_quality")) {
-						$("#revPerformance").append(line);
-					}
-					else if ((fea == "headset") || (fea == "processor") || (fea == "gpu") || (fea == "ram") || (fea == "internal_memory")) {
-						$("#revSpecs").append(line);
-					}
-					else if ((fea == "phone") || (fea == "recommendation") || (fea == "price")) {
-						$("#revOverall").append(line);
-					}
-					else {console.log('ERROR');}
+					//elems.sort(function(a,b) {	return parseFloat(b[0]) - parseFloat(a[0]) } );
+					elems.sort(function(a,b) {	return a[3].localeCompare(b[3]) } );
+					revData[section][0].push([fea,elems]);
 				}
+				console.log('revData:',revData);
+
+				for (var section in revData) {
+   				if (revData.hasOwnProperty(section) ) {
+		
+						var secData=revData[section];
+						//console.log(secData[2],secData[3],secData[4]);
+
+						var nums = secData[1];
+						nums.sort(function(a,b) {  return parseFloat(b) - parseFloat(a) } );
+						//li = get_font_list(num);
+						//console.log(nums);
+
+						var rows = secData[0];
+						for (var i=0; i<rows.length;i++) {
+							var fea=rows[i][0];
+							var elems=rows[i][1];
+							var line='<p>'.concat("<b>", fea.replace(/_/g," "), "</b>", ' : ');
+							for (var j=0; j<elems.length;j++) {
+								var elem=elems[j];
+								var num=elem[0];
+								//var fs = get_font(num,li);
+								var fs = get_font(num);
+								var adj=elem[1];
+								var href=elem[2];
+								var tone=elem[3];
+								adj=adj.concat('(', num,')');
+								var add="<a href=\"".concat(href, "\" style=\"color:", tone, ";font-size:", fs,  "px;\" data-toggle=\"modal\" data-target=\"#myModal\">  ", adj, "  </a>");
+								line=line.concat(add);
+							}
+							line=line.concat('</p>');
+							$(secData[5]).append(line);
+							//console.log(line);
+						}
+   				}
+				}
+
 		}
+
+		function get_font_list(num) {
+			return [.5,2.5,4,5,6.9];
+		}
+		function get_font(n) {
+			var num = Math.round(Math.log(n)*10);
+			var idx=[Math.abs(num-5), Math.abs(num-25), Math.abs(num-40), Math.abs(num-50), Math.abs(num-69)];
+			var i = idx.indexOf(Math.min.apply(Math, idx));
+			return fsizes[i];
+		}
+		function mean(num) {
+			var sum = num.reduce(function(a, b){return a+b;});
+			return round(sum % num.length);
+		}
+		
+		var fsizes=[12,14,16,18,20];
 
   }]);
 
@@ -186,3 +249,27 @@ angular.module('frontendApp')
 				$("#revBattery .panel-body").append(line);
 			}*/
 
+							/*if ((fea == "battery") || (fea == "battery_life")) {
+								$("#revBattery").append(line);
+							}
+							else if ((fea == "camera") || (fea == "camera_resolution") || (fea == "camera_flash") || (fea == "picture_quality") || (fea == "video_quality") || (fea == "low_light_performance") || (fea == "secondary_camera") || (fea == "primary_camera") ) {
+								$("#revCamera").append(line);
+							}
+							else if ((fea == "built_quality") || (fea == "body_covers") || (fea == "buttons")) {
+								$("#revBody").append(line);
+							}
+							else if ((fea == "display") || (fea == "display_size") || (fea == "display_resolution") || (fea == "viewing_angle") || (fea == "touch_response")) {
+								$("#revDisplay").append(line);
+							}
+							else if ((fea == "performance") || (fea == "apps") || (fea == "video") || (fea == "music") || (fea == "gaming") || (fea=="photography") || (fea == "sound_quality")) {
+								$("#revPerformance").append(line);
+							}
+							else if ((fea == "headset") || (fea == "processor") || (fea == "gpu") || (fea == "ram") || (fea == "internal_memory")) {
+								$("#revSpecs").append(line);
+							}
+							else if ((fea == "phone") || (fea == "recommendation") || (fea == "price")) {
+								$("#revOverall").append(line);
+							}
+							else {console.log('ERROR');}
+						}
+							*/
